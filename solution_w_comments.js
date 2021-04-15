66c97433668cb939c3b28c626fbdec50026f2deb
Ashley <ashleywong678@gmail.com>
2021-04-15 09:22:11 -0400

    Create solution_w_comments.js

---
+++ solution_w_comments.js
@@ -0,0 +1,16 @@
+function countElements(head) {
+   if(!head) return 0
+    
+    let count = 0
+    const queue = [head]
+    while(queue.length > 0){
+        // add one for the one we take out
+        const ele = queue.shift()
+        count += 1
+        // add to the queue if there's more
+       if(ele.next){
+           queue.push(ele.next)
+       }
+    }
+    return count
+}
